ce26d6b1e26b549830e1993372c51d5b9898ac7e
Tanguy <tanguy.demme@uavia.eu>
2019-08-20 15:03:54 +0200

    Update

--- app.js
+++ app.js
@@ -3,31 +3,103 @@ var path            = require("path");
 var watch           = require('node-watch');
 var OctoPrintServer = require('octoprint');
 var moveFile        = require('move-file');
-var colors = require('colors');
+var color			= require('./node_modules/nodeColor/color.js');
 
-
-var watchPath = 'C:\\Users\\Tanguy\\';
-var savePath  = 'C:\\Users\\Tanguy\\Documents\\gcode_history\\' ;
+var config			= require('./config.js');
 var lastSize  = -1;
 
+var server = new OctoPrintServer(config.octoprint);
 
-var settingsUlti = {
-	address: 'http://192.168.2.103:5000',
-    APIKey:'6540C84339734D8FA8425A7119FD5289'
+
+console.log('Home path : ' + config.paths.to_watch);
+console.log('Save path : ' + config.paths.gcode_history);
+
+ 
+
+watch(config.paths.to_watch, { recursive: true }, function(evt, name) {
+	//console.log('Changed: ' + name);
+
+	// Thing i dont want
+	if(	evt == 'removed'            ||
+		name.indexOf('.lnk')    > 0 || 
+		name.indexOf(config.paths.gcode_history)  > 0 ||
+		name.indexOf(config.paths.stl_history)  > 0 ||
+		name.indexOf('AppData') > 0 ){
+		return; 
+	}	
+	
+	// 
+	if( name.indexOf('.gcode') > 0 || name.indexOf('.stl')  > 0) {	
+		fs.stat(name, function(err, stats) {
+			if(err){
+				console.log('nopSize');
+				return;
+			}
+			
+			// File is GCODE
+			if( name.indexOf('.gcode') > 0 ) {
+				//Send dat shit
+				console.log('New GCODE find : ' + name + ' evt: ' + evt + ' size: ' + stats["size"]);
+				upload_to_octoprint(name, stats);
+			}
+
+			// File is STL
+			if( name.indexOf('.stl')  > 0 ) {
+				// Slice dat shit
+				console.log('New STL find : ' + name + ' evt: ' + evt + ' size: ' + stats["size"]);
+			}
+			
+		});
+	}
+	
+	
+});
+
+
+
+function upload_to_octoprint(name, stats){
+	
+	// Was already send
+	if (stats["size"] == lastSize || stats["size"] < 1){
+		console.log('This file has already been seen ' + name);
+		return;
+	}
+	lastSize = stats["size"];
+	
+	// Send file to 3d printer
+	console.log('uploading...');
+	server.sendFile(name).then(function(response){
+		console.log(response);		
+		console.log('Succes !');
+		//afterUpload(name);
+	}).catch(function(err){
+		console.error(err);
+	});
+	
 }
 
-var settingsCr10 = {
-	address: 'http://192.168.2.136:5000/',
-    APIKey:'86D6DB3DFB0F40C69215E74F27B0E02C'
+
+
+function afterUpload(file){
+	// Delet file ? Or move ?
+	var name = path.basename(file);
+	console.log('move ' + file + ' to ' + config.paths.gcode_history + name);
+	
+	//fs.rename(file, config.paths.gcode_history + name, function(err){
+	//	if(err){
+	//		console.log(err.red);
+	//	}
+		
+	//});
+	
+	try{
+		moveFile(file, config.paths.gcode_history + name);
+	}catch(err){
+		console.error('Move error', err);
+	}
+	
 }
 
-//var server = new OctoPrintServer(settingsUlti);
-var server = new OctoPrintServer(settingsCr10);
-
-
-
-console.log('Home path : '.cyan + watchPath.green);
-console.log('Save path : '.cyan + savePath.green);
 
 
 function getFilesizeInBytes(filename) {
@@ -37,20 +109,13 @@ function getFilesizeInBytes(filename) {
 }
 
 
+
+
+/*
+
 var sending = false;
 
-watch(watchPath, { recursive: true }, function(evt, name) {
-	//console.log('%s changed.', name);
 
-	//Thing i dont want
-	if(	evt == 'removed'            ||
-		name.indexOf('.lnk')    > 0 || 
-		name.indexOf(savePath)  > 0 ||
-		name.indexOf('AppData') > 0 ){
-		return; 
-	}
-	 
-	
 	//File is GCODE
 	if( name.indexOf('.gcode') > 0 ) {
 		fs.stat(name, function(err, stats) {
@@ -63,7 +128,7 @@ watch(watchPath, { recursive: true }, function(evt, name) {
 			}
 			
 			if (stats["size"] == lastSize || stats["size"] < 1){
-				console.log('This file has already been seen '.magenta + name);
+				console.log('This file has already been seen ' + name);
 				if(sending == false){
 					afterUpload(name);
 				}
@@ -73,55 +138,24 @@ watch(watchPath, { recursive: true }, function(evt, name) {
 			sending = true;
 			
 			//File is OK to be send
-			console.log('New Gcode find : ' + name.cyan + ' evt: ' + evt.cyan + ' size: ' + stats["size"]);
+			console.log('New Gcode find : ' + name + ' evt: ' + evt + ' size: ' + stats["size"]);
 			
 			//Send file to 3d printer
-			console.log('uploading...'.cyan);
+			console.log('uploading...');
 			server.sendFile(name).then(function(response){
 				console.log(response);		
-				console.log('Succes !'.green);
+				console.log('Succes !');
 				afterUpload(name);
 				sending = false;
-			}).catch(function(error){
-				console.log(error.red);
+			}).catch(function(err){
+				console.error(err);
 			});	
 		
 			
 			
 		});
 	}
-	
-
-	//File is STL
-	if( name.indexOf('.stl')  > 0 ) {
-		//Slice dat shit
-		console.log('New STL find : ' + name.cyan + ' evt: ' + evt.cyan);
-	}
+	*/
 	
 	
-});
-
-
-function afterUpload(file){
-	//Delet file ? Or move ?
-	var name = path.basename(file);
-	console.log('move ' + file.cyan + ' to ' + savePath.cyan + name.cyan);
-	/*
-	fs.rename(file, savePath + name, function(err){
-		if(err){
-			console.log(err.red);
-		}
-		
-	});*/
-	
-	try{
-		moveFile(file, savePath + name);
-	}catch(e){
-		console.log('Move error'.red);
-	}
-	
-}
-
-
-
-
+//console.log(process.cwd());
---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {
+	paths: {
+		to_watch		: 'C:\\Users\\Tanguy\\',
+		gcode_history  	: 'C:\\Users\\Tanguy\\Documents\\history\\gcode',
+		stl_history    	: 'C:\\Users\\Tanguy\\Documents\\history\\stl'
+	},
+	octoprint: {
+		address			: 'http://192.168.2.136:5000/',
+		APIKey			: '86D6DB3DFB0F40C69215E74F27B0E02C'
+	}
+};
+
+module.exports = config;
