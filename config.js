ee68d1e85cf29280ee4821f8acbd5cc9779c431c
Tanguy <tanguy.demme@uavia.eu>
2019-08-20 15:38:20 +0200

    fix

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 var config = {
 	paths: {
-		to_watch		: 'C:\\Users\\Pyro\\',
-		gcode_history  	: 'C:\\Users\\Pyro\\Documents\\gcode_history',
-		stl_history    	: 'C:\\Users\\Pyro\\Documents\\stl_history'
+		to_watch		: 'C:\\Users\\Tanguy\\',
+		gcode_history  	: 'C:\\Users\\Tanguy\\Documents\\history\\gcode',
+		stl_history    	: 'C:\\Users\\Tanguy\\Documents\\history\\stl'
 	},
 	octoprint: {
-		address			: 'http://176.132.206.163:5000/',
-		APIKey			: '86C6844C648F418C952E318F77D72843'
+		address			: 'http://192.168.2.136:5000/',
+		APIKey			: '86D6DB3DFB0F40C69215E74F27B0E02C'
 	}
 };
 
