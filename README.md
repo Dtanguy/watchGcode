3babb79165b35206404dcfe9f52b8d8fe824f646
dtanguy <demme.tanguy@gmail.com>
2020-12-27 02:11:23 +0100

    up

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 A daemon for simplifying STL / Gcode management for 3D printing with Octoprint.
 - Look in real time for new gcode file in a selected folder and upload it to Octoprint.
 - Keep history of the files in a selected folder with a timestamp in name.
-- [TODO] automatically slice STL files
-- [TODO] Start printing the last uploaded Gcode
+- [TODO] Automatically slice STL files
+- Start printing the last uploaded Gcode
+- [TODO] Small electron widget to select multiple printers and enable/disable PRINT_LAST_UPLOAD
 
 ## Installation
 ```js
@@ -14,10 +15,11 @@ npm i
 
 Rename 'sample.env' to '.env' and complete it as follows :
 ```env
-TO_WATCH='C:\Users\you'
-HISTORY='E:\Documents\'
 OCTOPRINT_ADRESS='http://xxx.xxx.xxx.xxx:5000'
 APIKEY='OCTOPRINT_API_KEY'
+TO_WATCH='C:\Users\you'
+HISTORY='E:\Documents\'
+PRINT_LAST_UPLOAD=TRUE
 ```
 
 ## Usage
--- app.js
+++ app.js
@@ -1,82 +1,90 @@
 require('dotenv').config();
-let fs				= require ('fs.extra');
-let path			= require("path");
-let watch			= require('node-watch');
-let OctoPrintServer	= require('octoprint');
-var colors 			= require('colors');
+const fs				= require('fs.extra');
+const path				= require("path");
+const colors 			= require('colors');
+const watch				= require('node-watch');
+//const OctoPrintServer	= require('octoprint');
+const OctoPrint 		= require('./octo-client');
+
 
 var settings = {
 	address: process.env.OCTOPRINT_ADRESS,
-	APIKey: process.env.APIKEY//,
-	//version: "0.1",
+	APIKey: process.env.APIKEY
 };
-let server = new OctoPrintServer(settings);
+//let server = new OctoPrintServer(settings);
+OctoPrint.setupAPI(settings);
 
-var GCODE_HISTORY = process.env.HISTORY + 'watchgcode\gcode';
-var STL_HISTORY = process.env.HISTORY + 'watchgcode\stl';
-if (!fs.existsSync(process.env.HISTORY + 'watchgcode')){
-    fs.mkdirSync(process.env.HISTORY + 'watchgcode');
-}
-if (!fs.existsSync(GCODE_HISTORY)){
-    fs.mkdirSync(GCODE_HISTORY);
-}
-if (!fs.existsSync(STL_HISTORY)){
-    fs.mkdirSync(STL_HISTORY);
-}
-
-let remoteFiles = {};
-console.log('Home path : ' + colors.green(process.env.TO_WATCH));
-console.log('Save path : ' + colors.green(GCODE_HISTORY));
+var BASE_HISTORY = path.join(process.env.HISTORY, "WatchGcode");
+var GCODE_HISTORY = path.join(BASE_HISTORY, "gcode");
+var STL_HISTORY = path.join(BASE_HISTORY, "stl");
+var startJobTimeout;
 
 
 
-
-watch(process.env.TO_WATCH, { recursive: true }, function(evt, name) {
+async function loop(evt, name){
 	//console.log('Changed: ' + name);
 
 	// Thing i don't want
-	if(	evt == 'removed'            ||
-		name.indexOf('.lnk')    > -1 || 
-		name.indexOf(GCODE_HISTORY)  > -1 ||
-		name.indexOf(STL_HISTORY)  > -1 ||
+	if(	evt == 'removed'            		||
+		name.indexOf('.lnk') > -1 			|| 
+		name.indexOf(GCODE_HISTORY) > -1 	||
+		name.indexOf(STL_HISTORY) > -1		||
 		name.indexOf('AppData') > -1 ){
 		return; 
 	}	
 	
-	// Thing i want
-	if( name.indexOf('.gcode') > -1 || name.indexOf('.stl')  > -1) {	
-		fs.stat(name, function(err, stats) {
+	// Is Size ok ?
+	var stats_;
+	await new Promise((resolve, reject) => {
+		fs.stat(name, function(err, stats) {	
 			if(err ||  stats["size"] < 1){
-				return;
-			}
-			
-			// File is GCODE
-			if( name.indexOf('.gcode') > -1 ) {
-				// Send it
-				console.log('New GCODE find : ' + colors.green(name) + ' evt: ' + evt + ' size: ' + stats["size"]);
-				sendOctoprint(name, stats);					
-			}
-
-			// File is STL
-			if( name.indexOf('.stl')  > -1 ) {
-				// Slice it
-				console.log('New STL find : ' + colors.green(name) + ' evt: ' + evt + ' size: ' + stats["size"]);
-				slice(name, stats);
-			}
-			
+				stats_ = -1;
+				//reject();
+			}else{
+				stats_ = stats;
+			}			
+			resolve();
 		});
-	}	
-	
-});
+	});
+	if(stats_ == -1){
+		//console.log("Ignore");
+		return;
+	}
+
+	// File is GCODE
+	if( name.indexOf('.gcode') > -1 ) {
+		// Send it
+		clearTimeout(startJobTimeout);
+		console.log('New GCODE find : ' + colors.green(name) + ' evt: ' + evt + ' size: ' + stats_["size"]);		
+		var res = false;
+		await new Promise((resolve, reject) => {
+			sendOctoprint(name, stats_, function(param){
+				res = param;
+				resolve();
+			});
+		});
+
+		// Print it
+		if(res && process.env.PRINT_LAST_UPLOAD == 'TRUE'){			
+			startJobTimeout = setTimeout(printLastUpload, 10000);				
+		}		
+	}
+
+	// File is STL
+	if( name.indexOf('.stl')  > -1 ) {
+		// Slice it
+		console.log('New STL find : ' + colors.green(name) + ' evt: ' + evt + ' size: ' + stats_["size"]);
+		slice(name, stats_);
+	}
+
+}
 
 
 
 
 
-
-function sendOctoprint(name, stats){
-	if(checkAlready(name, stats, function(){
-		
+async function sendOctoprint(name, stats, cb){
+	//if(checkAlready(name, stats, function(){		
 		//let date = new Date(Date.now());
 		//let new_name = path.join(GCODE_HISTORY, '[' + date.toISOString().replace(/T/, '_').replace(/\..+/, '').split(':').join('-') + ']_' + path.basename(name));
 		let new_name = path.join(GCODE_HISTORY, path.basename(name).split('.')[0] + '-[' + Date.now() + ']' + '.gcode');
@@ -84,12 +92,19 @@ function sendOctoprint(name, stats){
 		fs.move (name, new_name, function (err) {
 			if (err) {
 				console.error(err);
-				return;
+				cb(false);
 			}
 			console.warn('move to ' + colors.green(new_name));
-			upload_to_octoprint(new_name);	
+			//upload_to_octoprint(new_name, cb);
+			OctoPrint.filesUpload(new_name, function(res2){
+				console.log(res2);
+				if(res2.status == 'Success'){
+					cb(true);
+				}
+				cb(false);
+			});
 		});		
-	}));
+	//}));
 }
 
 
@@ -99,7 +114,7 @@ function slice(name, stats){
 
 
 
-
+/*
 function checkAlready(name, stats, callback){	
 	server.getAllFiles().then(function(files, err){
 		if (err) {
@@ -120,21 +135,93 @@ function checkAlready(name, stats, callback){
 		callback();		
 	});
 }
+*/
 
 
-
-
-function upload_to_octoprint(name){	
+/*
+async function upload_to_octoprint(name, cb){	
 	// Send file to 3d printer
 	console.warn('uploading... ');
-	server.sendFile(name).then(function(response){
-		//console.log(response);		
+	server.sendFile(name).then(function(response){	
 		console.log('Succes !');
-		//updateRemoteFiles();
+		cb(true);	
 	}).catch(function(err){
 		console.error(red("Can't upload to " + process.env.OCTOPRINT_ADRESS));
-		//console.error(err);
+		cb(false);
+	});					
+}
+*/
+
+
+
+function printLastUpload(){
+	//console.log("I print that");
+	OctoPrint.printerState(function(res){
+		//console.log(res);  
+
+		// Is Printer ready ?
+		try {
+			if(res.state.flags.operational && !res.state.flags.printing){}
+		} catch (error) {
+			console.log("Printer not ready");  
+			return;
+		}		
+	
+		// List files
+		console.log("Printer Ready"); 
+		OctoPrint.files(true, function(res2){
+			//console.log(res2);
+
+			let last = {};
+			try {
+				let lastIt = 0;
+				for (var i = 0; i < res2.files.length; i++){				
+					//console.log(res2.files[i].date);
+					if(res2.files[i].date > res2.files[lastIt].date){
+						lastIt = i;
+					}
+				}
+				last = res2.files[lastIt];
+				console.log("Selected: " + last.name);
+			} catch (error) {
+				console.log("Error listing gcode");  
+				return;
+			}
+			
+			OctoPrint.selectFile(last.name, false, function(response){
+				console.log(response);
+				console.log("Start print");
+			});
+
+		});
+
 	});
 }
 
 
+
+
+
+
+
+function main(){
+	// Create needed folders
+	if (!fs.existsSync(BASE_HISTORY)){
+		fs.mkdirSync(BASE_HISTORY);
+	}
+	if (!fs.existsSync(GCODE_HISTORY)){
+		fs.mkdirSync(GCODE_HISTORY);
+	}
+	if (!fs.existsSync(STL_HISTORY)){
+		fs.mkdirSync(STL_HISTORY);
+	}
+
+	// Resume
+	console.log('Home path : ' + colors.green(process.env.TO_WATCH));
+	console.log('Save path : ' + colors.green(GCODE_HISTORY));
+	console.log('Octoprint : ' + colors.green(process.env.OCTOPRINT_ADRESS));
+
+	// Start watch
+	watch(process.env.TO_WATCH, { recursive: true }, loop);
+}
+main();
